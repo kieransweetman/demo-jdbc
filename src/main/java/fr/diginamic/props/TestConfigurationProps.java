bfd12528a543732e68924cd2f5eedf8b921383d2
kieransweetman <kikisweetman@gmail.com>
2024-05-30 10:46:44 +0200

    w/ apache

--- src/main/java/fr/diginamic/props/TestConfigurationProps.java
+++ src/main/java/fr/diginamic/props/TestConfigurationProps.java
@@ -2,6 +2,10 @@ package fr.diginamic.props;
 
 import java.util.ResourceBundle;
 
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+
 /**
  * TestConfigurationProps
  */
@@ -9,7 +13,18 @@ public class TestConfigurationProps {
 
     public static void main(String[] args) {
         ResourceBundle configProperties = ResourceBundle.getBundle("config");
-
         System.out.println(configProperties.getString("database.url"));
+
+        Configurations config = new Configurations();
+        try {
+            Configuration properties = config.properties("config.properties");
+
+            System.out.println("With apache:");
+            System.out.println(properties.getString("database.url"));
+
+        } catch (ConfigurationException e) {
+            e.printStackTrace();
+        }
+
     }
 }
